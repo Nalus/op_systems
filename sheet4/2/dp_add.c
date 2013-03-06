372f27cdb66d70f28fa79d7bbf2eca32ef57a3c4
Konstantin Devyatov <kd79@hw.ac.uk>
2013-03-06 15:12:30 +0000

    1.2 parallel is complete

--- sheet4/2/dp_add.c
+++ sheet4/2/dp_add.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <time.h>
 
-#define N_THREADS 16
+#define N_THREADS 31
 #define SIZE 1000
 #define ITERATIONS 100000
 
@@ -14,13 +14,14 @@ struct timespec start, finish;
 typedef struct threadArgs threadArgs_t;
 
 struct threadArgs {
-  int tid;
+  pthread_t tid;
   int from;
   int to;
 };
 
 double a[SIZE];
 double b[SIZE];
+threadArgs_t** arg;
 
 pthread_barrier_t barrier;
 
@@ -33,44 +34,78 @@ xelapsed (struct timespec a, struct timespec b)
 }
 
 void sequential( )
-{ clock_gettime(CLOCK_REALTIME, &start);
-  int i,j;
+{ int i,j;
   for(j=0; j<ITERATIONS; j++)
   { for(i=0; i<SIZE; i++)
     {  a[i] = a[i] + b[i]; }
   }
-
-  clock_gettime(CLOCK_REALTIME, &finish);
 }
-
-
-
 
 void * simd( void *arg)
 {
-  int tid,from,to;
+  int tid,from,to,i;
 
   tid = ((threadArgs_t *)arg)->tid;
   from = ((threadArgs_t *)arg)->from;
   to = ((threadArgs_t *)arg)->to;
+
+  for(i=from; i<to; i++)
+  {  a[i] = a[i] + b[i]; }
 }
 
-void initData( )
+void initData()
 {
   int i;
   for( i=0; i<SIZE; i++) {
     a[i] = i;
     b[i] = (SIZE-1) - i;
   }
-  pthread_barrier_init( &barrier, NULL, N_THREADS);
+  //pthread_barrier_init( &barrier, NULL, N_THREADS);
 }
 
-int main()
-{ initData();sequential();
+//this function splits SIZE by number as equally as possible
+//and then returns an array of positions of chunk endings (to, including the last element)
+void splitting(int number)
+{ int rest,common,even_size,i;
 
+  rest = SIZE % number;
+  even_size = SIZE - rest;
+  common = even_size / number;
+
+  arg[0]->from = 0;
+  arg[0]->to = common;
+  if(rest > 0) { arg[0]->to = arg[0]->to + rest; }
+
+  for(i=1; i<number; i++)
+  { arg[i]->from = arg[i-1]->to;
+    arg[i]->to = arg[i]->from + common;
+  }
+}
+
+int main(int argc, char** argv)
+{ int k,l;
+  initData();
+  clock_gettime(CLOCK_REALTIME, &start);
+  if(argc != 2) { printf("Please provide one number < 30, to specify number of threads\n"); exit(EXIT_SUCCESS); }
+  int thread_n = atoi(argv[1]);
+  if(thread_n == 0 || thread_n > 30) { printf("Please make sure your nubmer is a valid integer <30\n"); exit(EXIT_SUCCESS); }
+
+  arg = malloc(sizeof(threadArgs_t*)*thread_n);
+  for(l=0; l<thread_n; l++) { arg[l] = malloc(sizeof(threadArgs_t)); }
+
+  splitting(thread_n);
+
+  for(k=0; k<ITERATIONS; k++)
+  { for(l=0; l<thread_n; l++)
+    { pthread_create(&arg[l]->tid,NULL,simd,arg[l]); }
+
+    for(l=0; l<thread_n; l++)
+    { pthread_join(arg[l]->tid, NULL); }
+  }
+
+  clock_gettime(CLOCK_REALTIME, &finish);
   fprintf (stderr, "Total time: %03li\n", xelapsed (finish, start));
-  return(0);
+
+  free(arg);
+  return(EXIT_SUCCESS);
 }
-
-  
-
